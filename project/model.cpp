06a1221998113a941bcdd51a02e7fc4ac863212b
Marco-Favaretto <favaretto801@gmail.com>
2023-08-20 19:22:39 +0200

    aggiunta curaVeleno e curaTossicita' nello use del model

--- project/model.cpp
+++ project/model.cpp
@@ -24,8 +24,22 @@ void model::use(Item *x) {
     overTime* ot = dynamic_cast<overTime*>(x);
 
     if(c && !ot) {
-        connect(c, SIGNAL(effectSignal(int)), player, SLOT(changeHP(int)));
-        c->effect();
+        if(c->getName() != "cure4poison" && c->getName() != "cure4toxic"){
+            connect(c, SIGNAL(effectSignal(int)), player, SLOT(changeHP(int)));
+            c->effect();
+        } else {
+            // ricerca overtime attivi
+            bool found = false;
+            for(Inventario::iteratore i = inventory.begin(); i != inventory.end() && !found; i++) {
+                overTime* it = dynamic_cast<overTime*>(&inventory[i]);
+                if(it && it->isActive()) {
+                    connect(it, SIGNAL(over(overTime*)), this, SLOT(stopOverTime(overTime*)));
+                    if((it->getName() == "toxic" && c->getName() == "cure4toxic")) { it->stopOT(); found = true;}
+                    if((it->getName() == "poison" && c->getName() == "cure4poison")) { it->stopOT(); found = true;}
+                }
+            }
+            if(player->getStatus() == POISONED) player->changeStatus(NORMAL);
+        }
     }
     else if (ot) {
         connect(ot, SIGNAL(effectSignal(int)), player, SLOT(changeHP(int)));
